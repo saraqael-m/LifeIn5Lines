07aefea81208985a164e974e21a0e4c8b1879f18
saraqael-m <80643194+saraqael-m@users.noreply.github.com>
2021-07-14 17:25:19 +0200

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,5 +1,5 @@
 import cv2, numpy as np, scipy.ndimage # import necessary libraries
-img = (cv2.cvtColor(cv2.imread("startDot.png"),cv2.COLOR_BGR2GRAY)/255+.5).astype("uint8") # get start image
+img = (cv2.cvtColor(cv2.imread("start.png"),cv2.COLOR_BGR2GRAY)/255+.5).astype("uint8") # get start image
 while cv2.waitKey(1) != 27: # main loop (ends when esc is pressed)
 	neighbor_count = scipy.ndimage.correlate(img,np.array([[1,1,1],[1,0,1],[1,1,1]]),mode="constant") # neighbor count of each cell
 	img = (np.logical_and((neighbor_count == 2) + (neighbor_count == 3), img) + np.logical_and(neighbor_count == 3, 1-img)).astype("uint8") # game of life rules
